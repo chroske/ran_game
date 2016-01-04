d7ab91aa9c6eb0346b995e6258b2acee7028ef58
chroske <chroske@chroske-no-MacBook-Air.local>
2016-01-05 01:30:10 +0900

    first commit

---
+++ Assets/scripts/PcButtonInput.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PcButtonInput : MonoBehaviour {
+
+    public GameObject player;
+
+    private PlayerController playerController;
+
+	// Use this for initialization
+	void Start () {
+        playerController = player.GetComponent<PlayerController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            playerController.MoveRight();
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            playerController.MoveLeft();
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow)) {
+            playerController.Jump();
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow)) {
+
+        }
+	}
+}
---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float MoveSpeed = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        MoveFoward();
+	}
+
+    private void MoveFoward (){
+        transform.position += transform.forward * Time.deltaTime * MoveSpeed;
+    }
+
+    public void MoveLeft (){
+        iTween.MoveBy(this.gameObject, iTween.Hash(
+            "x",-3,
+            "easeType",iTween.EaseType.linear,
+            "time",0.1f
+        ));
+    }
+
+    public void MoveRight (){
+        iTween.MoveBy(this.gameObject, iTween.Hash(
+            "x",3,
+            "easeType",iTween.EaseType.linear,
+            "time",0.1f
+        ));
+    }
+
+    public void Jump (){
+        iTween.MoveBy(this.gameObject, iTween.Hash(
+            "y",3,
+            "easeType",iTween.EaseType.linear,
+            "time",0.25f
+        ));
+    }
+
+        
+}
