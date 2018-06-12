b77ebbe216ebc243612cf2033c913ad1369c6b9a
sato <sato@kosuke.sato@ponos.co.jp>
2018-06-12 12:44:56 +0900

    爆弾投げて1秒すると敵が逃げるように

--- Assets/scripts/EnemyController.cs
+++ Assets/scripts/EnemyController.cs
@@ -8,9 +8,18 @@ public class EnemyController : MonoBehaviour {
 	GameObject ResultUI;
 	[SerializeField]
 	GameObject GameUI;
-
 	[SerializeField]
 	GameObject MainPcController;
+	[SerializeField]
+	Animator anim;
+
+	Vector3 granadePosition;
+	bool isGranadeAway = false;
+
+	void Awake ()
+	{
+		anim.SetFloat ("Speed", 1f);
+	}
 
 	void OnCollisionEnter (Collision col)
 	{
@@ -22,9 +31,47 @@ public class EnemyController : MonoBehaviour {
 		}
 	}
 
+	void OnTriggerEnter(Collider col){
+		if(col.gameObject.tag == "ExprosiveArea"){
+			Debug.Log ("do escape");
+			granadePosition = col.gameObject.transform.position;
+			Invoke ("Escape", 1.2f);
+		}
+	}
+
+	void OnTriggerExit(Collider col){
+		if(col.gameObject.tag == "ExprosiveArea"){
+			isGranadeAway = true;
+		}
+	}
+
 	void ShowResultUI(){
 		MainPcController.GetComponent<MainPcController> ().isGameOver = true;
 		ResultUI.SetActive (true);
 		GameUI.SetActive (false);
+	}
+
+	void Escape(){
+		if (!isGranadeAway) {
+			Debug.Log ("escape!!!");
+			anim.SetBool ("run", true);
+
+			transform.eulerAngles = new Vector3 (transform.eulerAngles.x, 130, transform.eulerAngles.z);
+
+			Hashtable parameters = new Hashtable ();
+			parameters.Add ("x", -14);
+			parameters.Add ("islocal", true);
+			parameters.Add ("easeType", iTween.EaseType.linear);
+			parameters.Add ("time", 1.5f);
+			parameters.Add ("oncomplete", "MoveFinish");
+			parameters.Add ("oncompletetarget", this.gameObject);
+			iTween.MoveBy (this.gameObject, parameters);
+		} else {
+			Debug.Log ("cansel escape");
+		}
+	}
+
+	void MoveFinish(){
+		anim.SetBool ("run", false);
 	}
 }
